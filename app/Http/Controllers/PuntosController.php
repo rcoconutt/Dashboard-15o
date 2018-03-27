d0d19bf92ba5b539c6198a863123e054624c8874
Ricardo Osorio <rk521@hotmail.com>
2018-03-27 14:12:16 -0600

    Fix ranking nulo

--- app/Http/Controllers/PuntosController.php
+++ app/Http/Controllers/PuntosController.php
@@ -5,45 +5,54 @@ namespace App\Http\Controllers;
 use App\Desgloce;
 use App\Dinamica;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Http\Request;
 
 class PuntosController extends Controller
 {
     public function topByMarca($dinamica_id) {
         try {
             $dinamica = Dinamica::where('ID_DINAMICA', $dinamica_id)->firstOrFail();
-            $desgloces = Desgloce::with('recibo', 'recibo.usuario')->where('ID_MARCA', $dinamica->marca_id)->get();
-            $users = collect();
-            foreach ($desgloces as $desgloce) {
-                $users->push($desgloce->recibo->usuario);
-            }
-
-            $users = $users->unique();
-
-            foreach ($users as $user) {
-                $total = 0;
-
+            if ($dinamica->marca_id > 0) {
+                $desgloces = Desgloce::with('recibo', 'recibo.usuario')->where('ID_MARCA', $dinamica->marca_id)->get();
+                $users = collect();
                 foreach ($desgloces as $desgloce) {
-                    if ($desgloce->recibo->usuario->ID_USUARIO == $user->ID_USUARIO) {
-                        $total += $desgloce->CANTIDAD;
-                    }
+                    $users->push($desgloce->recibo->usuario);
                 }
-                $user->total = $total;
+
+                $users = $users->unique();
+
+                foreach ($users as $user) {
+                    $total = 0;
+
+                    foreach ($desgloces as $desgloce) {
+                        if ($desgloce->recibo->usuario->ID_USUARIO == $user->ID_USUARIO) {
+                            $total += $desgloce->CANTIDAD;
+                        }
+                    }
+                    $user->total = $total;
+                }
+
+                $users = $users->sortByDesc(function ($product, $key) {
+                    return $product->total;
+                });
+
+                $users = $users->values()->all();
+
+                return response()->json([
+                    'success' => true,
+                    'message' => 'success',
+                    'data' => $users
+                ], 200);
+            } else {
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, "http://plisasegura.me/test/api.php/CTL_PUNTOS?include=CAT_PUNTO,CAT_USUARIO&transform=true&columns=CAT_PUNTO.VALOR,CAT_USUARIO.NOMBRE,CAT_USUARIO.ID_CENTRO7");
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                $output = curl_exec($ch);
+                curl_close($ch);
+
+                return response($output);
             }
-
-            $users = $users->sortByDesc(function ($product, $key) {
-                return $product->total;
-            });
-
-            $users = $users->values()->all();
-
-            return response()->json([
-                'success' => true,
-                'message' => 'success',
-                'data' => $users
-            ], 200);
         } catch (ModelNotFoundException $ex) {
-            return response()->json(['success' => false, 'message' => "No se encontró la dinámica"], 500);
+            return response()->json(['success' => false, 'message' => "No se encontró la dinámica"], 400);
         } catch (\Exception $ex) {
             return response()->json(['success' => false, 'message' => "Error, código 500" . $ex->getMessage()], 500);
         }
